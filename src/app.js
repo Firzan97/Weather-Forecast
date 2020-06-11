62aa024fa4046e8f971a713e09830520d5afdcf6
Firzan97 <FirzanAzrai97@gmail.com>
2020-06-11 10:11:39 +0800

    setup env for heroku deployment

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const templatePath= path.join(__dirname,"../templates/views")
 const partialPath = path.join(__dirname,"../templates/partials")
 
 const app = express()
+const port = process.env.port || 3000
 
 //define path for express directory
 app.set('view engine', 'hbs')
@@ -117,6 +118,6 @@ app.get('*', (req,res)=>{
    })
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Server is up on port 3000")
 })
